f4ebd175d6554a8f309334c3a8243e40badf229d
nicholasjamessmith <njsmith6@gmail.com>
2024-01-26 19:14:03 -0500

    basic server set up

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Basic server setup
+//Import dependencies 'express' and types 'Request' and 'Response' from express
+var express = require('express');
+//Import .env info
+require("dotenv").config();
+//Create an application function
+var app = express();
+//Send message as a get request to '/'
+app.get('/', function (req, res) {
+    res.send("Hello, World!");
+});
+//Listen on PORT or port 3000
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () { console.log("Listening on port ".concat(PORT)); });
---
+++ server.ts
@@ -0,0 +1,19 @@
+//Basic server setup
+//Import dependencies 'express' and types 'Request' and 'Response' from express
+const express = require('express')
+import { Request, Response } from 'express'
+//Import .env info
+require("dotenv").config()
+
+
+//Create an application function
+const app = express()
+
+//Send message as a get request to '/'
+app.get('/', (req: Request, res: Response) => {
+  res.send("Hello, World!")
+})
+
+//Listen on PORT or port 3000
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) })
